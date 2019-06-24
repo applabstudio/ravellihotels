feaeaed3aabfd18ab0789ef8af9c2dc3086a54a7
silver <silver.designer.developer@gmail.com>
2019-06-24 15:04:10 +0200

    Table Update

--- dev/js/modules/module.js
+++ dev/js/modules/module.js
@@ -1,6 +1,6 @@
 const module = function() {
 	const table = document.querySelectorAll('table');
-	table.forEach((t, i=0) => t.className = `uk-table uk-table-divider`);
+	table.forEach((t, i=0) => t.className = `uk-table uk-table-divider uk-table-striped uk-table-hover uk-table-responsive`);
 }
   
 export default { init:module };
