a9543c36dd1cd3e43e33a6f5e7980eb2c786e618
silver <silver.designer.developer@gmail.com>
2019-06-25 12:03:23 +0200

    Updates

--- dev/js/modules/site-header.js
+++ dev/js/modules/site-header.js
@@ -1,6 +1,7 @@
 const dom = {
   siteHeader: '.uk-navbar',
   siteNav: '.js-site-nav',
+  toTop: '.toTop',
 };
 
 const setScrollClass = (evt) => {
@@ -12,8 +13,18 @@ const setScrollClass = (evt) => {
   }
 }
 
+const setVisibleScrollToTop = (evt) => {
+  const toTop = document.querySelector(dom.toTop);
+  if (window.pageYOffset > 250) {
+    toTop.classList.remove('uk-hidden');
+  } else {
+    toTop.classList.add('uk-hidden');
+  }
+}
+
 const events = () => {
   window.addEventListener('scroll', setScrollClass);
+  window.addEventListener('scroll', setVisibleScrollToTop);
 };
 
 export default {
