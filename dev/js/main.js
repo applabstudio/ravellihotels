d16007442e05b9e3be2a8a7f2fbb78d53f8866b1
silver <silver.designer.developer@gmail.com>
2019-06-25 17:42:47 +0200

    Updates markup and styles

--- dev/js/main.js
+++ dev/js/main.js
@@ -2,36 +2,17 @@ import 'lazysizes';
 import siteHeader from './modules/site-header';
 import allclick from './modules/allclick';
 import modal from './modules/modal';
-import module from './modules/module';
+import setClassUiKit from './modules/setClassUiKit';
+import hamburgher from './modules/hamburgher';
 import {MDCTextField} from '@material/textfield';
 
 siteHeader.init();
 allclick.init();
 modal.init();
-module.init();
-
-hamburgher();
-
-function hamburgher() {
-  let logo = document.querySelector('#logo-open');
-  let logoh4 = document.querySelector('#logoh4');
-    document.querySelector('#logo-open').addEventListener('click', function() {
-      logo.style.visibility = 'hidden';
-      logoh4.style.visibility = 'hidden';
-    });
-    document.querySelector('.uk-close').addEventListener('click', function() {
-      logo.style.visibility = 'visible';
-      logoh4.style.visibility = 'visible';
-    });
-}
+setClassUiKit.init();
+hamburgher.init();
 
 const inps = document.querySelectorAll('.mdc-text-field');
 inps.forEach(element => {
   new MDCTextField(element);
 });
-
-//Dynamic set class and attributes UIKit
-if(document.querySelectorAll("#bg-section").length > 0) {
-  document.getElementById('bg-section').getElementsByTagName('img')[0].className += "uk-animation-reverse uk-transform-origin-top-right";
-  document.getElementById('bg-section').getElementsByTagName('img')[0].setAttribute("uk-scrollspy", "cls: uk-animation-kenburns; repeat: true"); 
-}
