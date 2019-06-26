86418a4844ab65e5eb975cdd99429c142ceee3af
silver <silver.designer.developer@gmail.com>
2019-06-26 15:27:16 +0200

    Updates styles. markup and js

--- dev/js/main.js
+++ dev/js/main.js
@@ -2,17 +2,18 @@ import 'lazysizes';
 import siteHeader from './modules/site-header';
 import allclick from './modules/allclick';
 import modal from './modules/modal';
-import setClassUiKit from './modules/setClassUiKit';
+import UIKit from './modules/UIKit';
 import hamburgher from './modules/hamburgher';
 import {MDCTextField} from '@material/textfield';
 
 siteHeader.init();
 allclick.init();
 modal.init();
-setClassUiKit.init();
+UIKit.init();
 hamburgher.init();
 
-const inps = document.querySelectorAll('.mdc-text-field');
-inps.forEach(element => {
+// Instance MDCTextField
+const textfieldMaterialDesign = document.querySelectorAll('.mdc-text-field');
+textfieldMaterialDesign.forEach(element => {
   new MDCTextField(element);
 });
