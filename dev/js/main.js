a5ac3e3281635f769294f451cd8124e2aed030f4
silver <silver.designer.developer@gmail.com>
2019-07-02 16:13:24 +0200

    * STABLE VERSION *

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,16 +1,14 @@
 import 'lazysizes';
 import siteHeader from './modules/site-header';
 import allclick from './modules/allclick';
-import modal from './modules/modal';
 import UIKit from './modules/UIKit';
 import hamburgher from './modules/hamburgher';
 import {MDCTextField} from '@material/textfield';
 
 siteHeader.init();
 allclick.init();
-modal.init();
-UIKit.init();
 hamburgher.init();
+UIKit.init();
 
 // Instance MDCTextField
 const textfieldMaterialDesign = document.querySelectorAll('.mdc-text-field');
