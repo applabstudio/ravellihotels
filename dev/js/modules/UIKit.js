aeb0ca97ce404db3dd52464ee1c408bbd4df87df
silver <silver.designer.developer@gmail.com>
2019-07-05 10:16:45 +0200

    updates forms

--- dev/js/modules/UIKit.js
+++ dev/js/modules/UIKit.js
@@ -4,6 +4,16 @@ const UIKit = function() {
 	// Uk Table
 	const table = document.querySelectorAll('table');
 	table.forEach((t, i=0) => t.className = `uk-table uk-table-divider uk-table-striped uk-table-hover uk-table-responsive`);
+
+	// form stay-request
+	if(document.querySelectorAll(".stay-request").length > 0) {
+		document.getElementById('stay-request-form-wrapper').getElementsByTagName('form')[0].className += "stay-request";
+	}
+
+	// form contact
+	if(document.querySelectorAll("#contact").length > 0) {
+		document.getElementById('contact').getElementsByTagName('form')[0].className += "mdc-contact";
+	}
 	
 	// Do you have problems with inputs? Add the classes from this if block
 	// if(document.querySelectorAll("#contact").length > 0) {
@@ -13,7 +23,7 @@ const UIKit = function() {
 
 	// Drupal Menu
 	/* If you want to add preprocess classes with hooks
-	$variables['content']['links']['#attributes']['class'][] = 'list-unstyled'; */
+	$variables['content']['links']['#attributes']['class'][] = 'myclass'; */
 	var everyChild = document.querySelectorAll("#drupal_menu li a");
 	for (var i = 0; i < everyChild.length; i++) {
 		everyChild[i].classList.add("uk-button-text");
